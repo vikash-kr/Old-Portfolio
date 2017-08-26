2a5114ad8c12f1656506e7201920b08df177f1e4
vikash kumar <iit2016041@iiita.ac.in>
2017-08-27 02:30:12 +0530

    Make scroll smooth

--- jQuery.js
+++ jQuery.js
@@ -1,6 +1,16 @@
+//for toggle
 jQuery(function () {
     var $div = $('.btn-1');
     setInterval(function () {
         $div.toggleClass('btn-1 btn-2');
     }, 1000);
 })
+// Make scroll smooth
+$('a[href^="#"]').click(function(){
+	var the_id = $(this).attr("href");
+
+	$('html, body').animate({
+		scrollTop:$(the_id).offset().top
+	}, 'slow');
+	return false;
+});
